28e3980b68679f712556141aa934cddc1591c275
Daniel Lea <limiaofei@difengshanguo.com>
2018-05-16 19:53:55 +0800

    init

---
+++ cloud-redis-examples/cloud-redis-examples-order/src/main/java/org/godbles4me/cloud/redis/examples/order/CloudRedisExamplesOrderApplication.java
@@ -0,0 +1,12 @@
+package org.godbles4me.cloud.redis.examples.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudRedisExamplesOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudRedisExamplesOrderApplication.class, args);
+	}
+}
---
+++ cloud-redis-examples/cloud-redis-examples-stock/src/main/java/org/godbles4me/cloud/redis/examples/stock/CloudRedisExamplesStockApplication.java
@@ -0,0 +1,12 @@
+package org.godbles4me.cloud.redis.examples.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudRedisExamplesStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudRedisExamplesStockApplication.class, args);
+	}
+}
